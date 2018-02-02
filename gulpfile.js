2e313be533164a73387b94c6cb9ec34d511d8d7f
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-02-02 15:50:36 -0500

    added autoprefixer, cssvars, nested

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+const gulp = require('gulp'),
+      watch = require('gulp-watch'),
+      postcss = require('gulp-postcss'),
+      autoprefixer = require('autoprefixer'),
+      cssvars = require('postcss-simple-vars'),
+      nested = require('postcss-nested');
+
+gulp.task('default', () => {
+  console.log('!!!');
+});
+
+gulp.task('html', () => {
+  console.log('HTML file changed!!');
+});
+
+gulp.task('styles', () => {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () => {
+  watch('./app/index.html', () => {
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/styles.css', () => {
+    gulp.start('styles');
+  });
+});
