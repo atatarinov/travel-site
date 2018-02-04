0f670eb432e8e6c330d948c74c25c35b087b2dc7
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-02-04 00:44:55 -0500

    added mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,15 @@ const gulp = require('gulp'),
       autoprefixer = require('autoprefixer'),
       cssvars = require('postcss-simple-vars'),
       nested = require('postcss-nested'),
-      cssImport = require('postcss-import');
+      cssImport = require('postcss-import'),
+      mixins = require('postcss-mixins');
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(error) {
+      console.log(error.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
