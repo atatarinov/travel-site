745e1e0c2cc2324e5e30ed6ea6d657574c3f4007
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-02-03 20:40:31 -0500

    refactored css

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ const gulp = require('gulp'),
       postcss = require('gulp-postcss'),
       autoprefixer = require('autoprefixer'),
       cssvars = require('postcss-simple-vars'),
-      nested = require('postcss-nested');
+      nested = require('postcss-nested'),
+      cssImport = require('postcss-import');
 
 gulp.task('default', () => {
   console.log('!!!');
@@ -15,7 +16,7 @@ gulp.task('html', () => {
 
 gulp.task('styles', () => {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
@@ -23,7 +24,7 @@ gulp.task('watch', () => {
   watch('./app/index.html', () => {
     gulp.start('html');
   });
-  watch('./app/assets/styles/styles.css', () => {
+  watch('./app/assets/styles/**/*.css', () => {
     gulp.start('styles');
   });
 });
